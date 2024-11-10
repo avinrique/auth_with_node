4a15232a9c6e67c2c40ab5e14510a50892516c27
avviinrique <avigupta2001ad@gmail.com>
2024-11-11 02:29:48 +0530

    auth system with email added

---
+++ controllers/homecontroller.js
@@ -0,0 +1,11 @@
+exports.getcontrol = (req,res)=>{
+    if(req.isAuthenticated()){
+        res.render('home', {logged_in :"false"
+    })
+    }
+    else{
+        res.render('home' ,{logged_in :"true"
+        })
+    }
+    
+}
